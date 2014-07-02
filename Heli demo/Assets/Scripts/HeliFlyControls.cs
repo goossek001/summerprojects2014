603c827982da07903180d9c581d2de033e66eec2
Kay Goossen <kay.goossen@hva.nl>
2014-07-02 22:22:07 +0200

    Changed the controls and weight of the heli

--- Heli demo/Assets/Scripts/HeliFlyControls.cs
+++ Heli demo/Assets/Scripts/HeliFlyControls.cs
@@ -10,8 +10,8 @@ public class HeliFlyControls : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		maxUpwardsForce = 15;
-		maxDownwardsForce = 4;
+		maxUpwardsForce = 12;
+		maxDownwardsForce = 2;
 	}
 	
 	// Update is called once per frame
@@ -32,19 +32,19 @@ public class HeliFlyControls : MonoBehaviour {
 
 		//Update pitch and roll
 		float currentPitch = rigidbody.rotation.eulerAngles.x;
-		if (currentPitch > 180) currentPitch = -(currentPitch - 360);
+		if (currentPitch > 180) currentPitch -= 360;
 		float currentRoll = rigidbody.rotation.eulerAngles.z;
-		if (currentRoll > 180) currentRoll = (currentRoll - 360);
+		if (currentRoll > 180) currentRoll -= 360;
 		//Rotate back toward no pitch and roll
-		rigidbody.AddRelativeTorque(0.001f*currentPitch, 0, 0.001f*-currentRoll);
+		rigidbody.AddRelativeTorque(0.002f*-currentPitch*rigidbody.mass, 0, 0.002f*-currentRoll*rigidbody.mass);
 
 		float pitchInput = Mathf.Clamp(Input.GetAxis("Pitch"), -1, 1);
 		float rollInput = Mathf.Clamp(Input.GetAxis("Roll"), -1, 1);
-		rigidbody.AddRelativeTorque(0.2f*pitchInput, 0, 0.2f*rollInput);
+		rigidbody.AddRelativeTorque(0.2f*pitchInput*rigidbody.mass, 0, 0.2f*rollInput*rigidbody.mass);
 
 		//Update yaw
 		float yawInput = Input.GetAxis("Yaw");
-		rigidbody.AddRelativeTorque(0, 0.3f*yawInput, 0);
+		rigidbody.AddRelativeTorque(0, 0.3f*yawInput*rigidbody.mass, 0);
 
 	}
 }
