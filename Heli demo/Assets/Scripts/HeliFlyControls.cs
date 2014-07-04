1ddd226c19ec3987705ef2270f5d52ee64dd621c
Kay Goossen <kay.goossen@hva.nl>
2014-07-04 11:30:18 +0200

    changed ground texture to grass

--- Heli demo/Assets/Scripts/HeliFlyControls.cs
+++ Heli demo/Assets/Scripts/HeliFlyControls.cs
@@ -9,35 +9,43 @@ public class HeliFlyControls : MonoBehaviour {
 	float rotationBackForce;
 
 	// Use this for initialization
-	void Start () {
+	public void Start () {
 		maxUpwardsForce = 25;
 		maxDownwardsForce = 11;
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-		//Update lift
+	public void FixedUpdate () {
+		UpdateLift();
+		UpdateRotation();
+	}
+
+	//Add force in the vertical direction of the chopper
+	private void UpdateLift() {
 		float lift = Input.GetAxis("Lift");
 		float verticalForce;
 		if (lift == 0) {
 			verticalForce = -Physics.gravity.y;
 		} else if (lift > 0) {
-			verticalForce = lift * maxUpwardsForce;
+			verticalForce = lift * maxUpwardsForce * rigidbody.mass;
 		} else {
-			verticalForce = lift * maxDownwardsForce;
+			verticalForce = lift * maxDownwardsForce * rigidbody.mass;
 		}
-		verticalForce *= rigidbody.mass;
+
 		Vector3 force = new Vector3 (0, verticalForce, 0);
 		rigidbody.AddRelativeForce(force);
+	}
 
-		//Update pitch and roll
+	//Update rotation in all 3 directions
+	private void UpdateRotation() {
+		//Slowly rotate back to 0 roll and pitch
 		float currentPitch = rigidbody.rotation.eulerAngles.x;
 		if (currentPitch > 180) currentPitch -= 360;
 		float currentRoll = rigidbody.rotation.eulerAngles.z;
 		if (currentRoll > 180) currentRoll -= 360;
-		//Rotate back toward no pitch and roll
 		rigidbody.AddRelativeTorque(0.01f*-currentPitch*rigidbody.mass, 0, 0.01f*-currentRoll*rigidbody.mass);
 
+		//Roll and pitch
 		float pitchInput = Mathf.Clamp(Input.GetAxis("Pitch"), -1, 1);
 		float rollInput = Mathf.Clamp(Input.GetAxis("Roll"), -1, 1);
 		rigidbody.AddRelativeTorque(4f*pitchInput*rigidbody.mass, 0, 4f*rollInput*rigidbody.mass);
@@ -45,6 +53,5 @@ public class HeliFlyControls : MonoBehaviour {
 		//Update yaw
 		float yawInput = Input.GetAxis("Yaw");
 		rigidbody.AddRelativeTorque(0, 6f*yawInput*rigidbody.mass, 0);
-
 	}
 }
