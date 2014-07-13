e2543aecea05abce8b100b138125ba4a970c7fae
Kay Goossen <kay.goossen@hva.nl>
2014-07-13 11:57:20 +0200

    Added a game where you can swim with a fish

---
+++ Fish/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : Swimming {
+
+	public void FixedUpdate () {
+		float input = Input.GetAxis ("Horizontal");
+		if (input != 0) {
+			Swim(input);
+		}
+	}
+}
---
+++ Fish/Assets/Scripts/Swimming.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Make a fish tail move and give with this give the fish force and give the fish torque as result of water moving against the tail when the fish is moving 
+abstract public class Swimming : MonoBehaviour {
+
+	//The fish body parts that move seperately
+	public GameObject body;
+	public GameObject[] tail;
+
+	//The swimming propeties
+	public float tailPower = 20;
+	public float swimPower = 6;
+
+	private HingeJoint2D[] joints;
+
+	public void Start () {
+		joints = new HingeJoint2D[tail.Length];
+
+		joints[0] = body.GetComponent<HingeJoint2D>();
+		for (int i = 1; i < joints.Length; i++) {
+			joints[i] = tail[i-1].GetComponent<HingeJoint2D>();
+		}
+	}
+
+	protected void Swim (float input) {
+		float torque = input * tailPower;
+		for (int i = 0; i < tail.Length; i++) {
+			if (isJointFullyTurned(joints[i])) {
+				tail[i].rigidbody2D.AddTorque(torque);
+			}
+		}
+		body.rigidbody2D.AddTorque(-torque*tail.Length*1.75f);
+
+		if (isJointFullyTurned(joints[0])) {
+			float power = (body.rigidbody2D.velocity - tail [tail.Length - 1].rigidbody2D.velocity).magnitude * swimPower;
+			body.rigidbody2D.AddRelativeForce(new Vector2(power, 0));
+		}
+	}
+
+	private static bool isJointFullyTurned(HingeJoint2D joint) {
+		return joint.jointAngle * 0.9f < joint.limits.max && joint.jointAngle * 0.9f > joint.limits.min;
+	}
+}
